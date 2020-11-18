283566aceecc9a6f85c99f8fd7d047391da67ea0
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-11-18 17:20:37 -0500

    Proyecto Base

---
+++ Assets/Shaders/Assets/OverlayTextureChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class OverlayTextureChange : MonoBehaviour
+{
+    public ScreenOverlay screenO;
+    public float interval = 0.02f;
+    public Texture2D[] textures = new Texture2D[0];
+    private int cantidadText = 0;
+
+    private const string _CambioTextura = "CambioTextura";
+
+    private void Start()
+    {
+        InvokeRepeating(_CambioTextura, 0, interval);
+    }
+
+    public void CambioTextura()
+    {
+        screenO.texture = textures[cantidadText];
+        cantidadText++;
+        if (cantidadText == textures.Length)
+        {
+            cantidadText = 0;
+        }
+    }
+}
---
+++ Assets/Shaders/ChangeTubeMaterial.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ChangeTubeMaterial : MonoBehaviour
+{
+    public Material mat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
