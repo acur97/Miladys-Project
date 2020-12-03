528883d58888d04ace043096faf6501d6dbc0f59
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-12-03 16:37:25 -0500

    Foveated Rendering

---
+++ Assets/_Scripts/FoveatedRendering.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static OVRManager;
+
+public class FoveatedRendering : MonoBehaviour
+{
+    public FixedFoveatedRenderingLevel Rlevel = FixedFoveatedRenderingLevel.Low;
+    public bool DynamicFixedFoveatedRendering = true;
+
+    private void Awake()
+    {
+        OVRManager.fixedFoveatedRenderingLevel = Rlevel; // it's the maximum foveation level
+        OVRManager.useDynamicFixedFoveatedRendering = true;
+    }
+}
