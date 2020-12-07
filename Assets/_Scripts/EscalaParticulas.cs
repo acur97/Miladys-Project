ef95d5c1de1a81f84331f5c3aafd02c957d897b7
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-12-07 18:59:03 -0500

    no message

---
+++ Assets/_Scripts/EscalaParticulas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscalaParticulas : MonoBehaviour
+{
+    public Transform esfera;
+    public ParticleSystem ps;
+    public AudioSpectrum aSpectrum;
+    public AudioSource aSource;
+    public float multiplicador = 1;
+    public float _base = 0.25f;
+
+    private float valor;
+
+    private void Update()
+    {
+        valor = (_base + (aSpectrum.amplitudeBuffer * multiplicador)) * aSource.volume;
+        esfera.localScale = new Vector3(valor, valor, valor);
+        var sh = ps.shape;
+        var mn = ps.main;
+        sh.radius = valor;
+        mn.startSpeed = ((valor - _base) * 2) * aSource.volume;
+    }
+}
